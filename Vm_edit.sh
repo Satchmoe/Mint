4b79cfb53da7760ef9e6df0bfa5434937cc1a768
Satchmoe <carl_chevelle_350@hotmail.com>
2017-06-13 12:22:55 -0500

    Vitual-Memory-edit

--- Vm_edit.sh
+++ Vm_edit.sh
@@ -3,7 +3,7 @@
 # Tested on: 18.1
 # Run as root.......
 
-echo -n "vm.swappiness= 10" >> /etc/sysctl.conf
-echo -n "vm.dirty_ratio = 15" >> /etc/sysctl.conf
-echo -n "vm.vfs_cache_pressure = 70" >> /etc/sysctl.conf
-echo -n "vm.dirty_background_ratio = 5" >> /etc/sysctl.conf
+echo "vm.swappiness= 10" >> /etc/sysctl.conf
+echo "vm.dirty_ratio = 15" >> /etc/sysctl.conf
+echo "vm.vfs_cache_pressure = 70" >> /etc/sysctl.conf
+echo "vm.dirty_background_ratio = 5" >> /etc/sysctl.conf
