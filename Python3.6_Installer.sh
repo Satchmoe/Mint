eda78c7872be0df263c12685631ea6ee898e08c8
Satchmoe <carl_chevelle_350@hotmail.com>
2017-06-12 21:52:43 -0500

    Install Python3.6 for 16.04 release

---
+++ Python3.6_Installer.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# To install "Python 3.6" on Linux Mint 18.*... The 16.04 releas
+# Run this to run the "installer.py"
+
+sudo add-apt-repository ppa:fkrull/deadsnakes
+sudo apt-get update
+sudo apt-get install python3.6
