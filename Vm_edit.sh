0282d15aba18adda7fbaeb2905a502a84ea5c997
Satchmoe <carl_chevelle_350@hotmail.com>
2017-06-14 12:42:20 -0500

    Vitual-Memory-Edit

--- Vm_edit.sh
+++ Vm_edit.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 # Edit vitual memory for Linux Mint
-# Tested on: 18.1
+# Tested on: 18.1 = worked
 # Run as root.......
 
-echo "vm.swappiness= 10" >> /etc/sysctl.conf
+echo "vm.swappiness = 10" >> /etc/sysctl.conf
 echo "vm.dirty_ratio = 15" >> /etc/sysctl.conf
 echo "vm.vfs_cache_pressure = 70" >> /etc/sysctl.conf
 echo "vm.dirty_background_ratio = 5" >> /etc/sysctl.conf
